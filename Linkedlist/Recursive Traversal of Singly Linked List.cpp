c131d2802783114cb5a1e618dc87b9428ed065e3
subhajitpatra498 <subhajitpatra498@gmail.com>
2021-10-06 22:33:49 +0530

    start linkedlist

---
+++ Linkedlist/Recursive Traversal of Singly Linked List.cpp
@@ -0,0 +1,28 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+struct Node{
+int data;
+Node* next;
+Node(int x)
+{
+    data=x;
+    next=NULL;
+}
+};
+void rprint(Node* head)
+{
+    if(head==NULL)
+        return;
+    cout<<(head->data)<<" ";
+    rprint(head->next);
+}
+int main(){
+    Node* head=new Node(10);
+    head->next=new Node(20);
+    head->next->next=new Node(20);
+    head->next->next->next=new Node(20);
+    rprint(head);
+
+}
