b782b521bf59b360c28d8d96e83c4aaf27ba6e91
subhajitpatra498 <subhajitpatra498@gmail.com>
2021-10-18 20:18:40 +0530

    insert end of circular linkedlist

---
+++ Linkedlist/Insert_at_begin_of_circular_linkedlist.cpp
@@ -0,0 +1,77 @@
+#include <bits/stdc++.h> 
+using namespace std; 
+
+struct Node{
+    int data;
+    Node* next;
+    Node(int d){
+        data=d;
+        next=NULL;
+    }
+};
+Node* insertbegin(Node*head,int data)
+// {
+//     Node*temp=new Node(data);
+//     if(head==NULL)
+//     {
+//         temp->next=temp;
+//         return temp;
+//     }
+//     else{
+//         Node*curr=head;         //naive solution ,time complexity O(n)
+//         while (curr->next!=head)
+//         {
+//             /* code */
+//             curr=curr->next;
+//         }
+//         curr->next=temp;
+//         temp->next=head;
+//         return temp;
+        
+//     }
+// }
+{
+        Node*temp=new Node(data);
+    if(head==NULL)
+    {
+        temp->next=temp;
+        return temp;
+    }
+    else{
+        temp->next=head->next;  //efficient solution time complexity O(1)
+        head->next=temp;
+        int swap=head->data;
+        head->data=temp->data;
+        temp->data=swap;
+            return head;
+    }
+
+}
+void print(Node *head)
+{
+    if(head==NULL)
+    {
+        return ;
+    }
+    Node* temp=head;
+    do
+    {
+        cout<<temp->data<<" ";
+        temp=temp->next;
+    } while (temp!=head);
+    
+    
+}
+int main() 
+{ 
+	Node *head=new Node(10);
+	head->next=new Node(5);
+	head->next->next=new Node(20);
+	head->next->next->next=new Node(15);
+	head->next->next->next->next=head;
+    print(head);
+    cout<<"\n";
+     head=insertbegin(head,89);
+    print(head);
+	return 0;
+} 
